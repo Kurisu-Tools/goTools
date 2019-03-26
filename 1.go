fd1542232b1386bda9f34f0a83ce1daab1c7c919
Amatist Kurisu <misaki.zhcy@gmail.com>
2019-03-26 21:39:55 +0800

    init

--- 1.go
+++ 1.go
@@ -1,7 +1,16 @@
 package go_Tools
 
-func InArray(arr []interface{}, str string) bool {
-	for _, value := range arr {
+func InStringArray(arr *[]string, str string) bool {
+	for _, value := range *arr {
+		if value == str {
+			return true
+		}
+	}
+	return false
+}
+
+func InIntArray(arr *[]int, str int) bool {
+	for _, value := range *arr {
 		if value == str {
 			return true
 		}
